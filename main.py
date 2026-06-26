8f7bed71d14580d8f77cc6dd41ebf83a88e5d9cf
Lasckuer <abobablack09@gmail.com>
2026-06-27 00:25:45 +0300

    Добавить поддержку прокси и Redis в конфигурацию бота

--- main.py
+++ main.py
@@ -1,21 +1,51 @@
 import os
 import asyncio
 from aiogram import Bot, Dispatcher
+from aiogram.client.session.aiohttp import AiohttpSession
+from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.fsm.storage.redis import RedisStorage
 from dotenv import load_dotenv
-from app import containers
-from app.handlers import messages
+from app.handlers import messages, containers
 from app.handlers.scheduler import metrics_refresher
 from logger import logger
 
 load_dotenv()
+
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+PROXY_URL = os.getenv("PROXY_URL")
+REDIS_URL = os.getenv("REDIS_URL")
 
 if not BOT_TOKEN:
     raise ValueError("BOT_TOKEN не задан в конфигурации .env!")
 
+def log_redis_on_info():
+    logger.info(f"✅ Успешно подключено к Redis по адресу: {REDIS_URL}")
+
+def log_redis_off_info():
+    logger.warning("⚠️ REDIS_URL не найден. Бот использует MemoryStorage (данные будут сброшены при рестарте).")
+
 async def main():
-    bot = Bot(token=BOT_TOKEN)
-    dp = Dispatcher()
+    session = None
+    
+    if PROXY_URL:
+        if PROXY_URL.startswith("socks"):
+            from aiohttp_socks import ProxyConnector
+            connector = ProxyConnector.from_url(PROXY_URL)
+            session = AiohttpSession(connector=connector)
+            logger.info("Используется SOCKS прокси.")
+        else:
+            session = AiohttpSession(proxy=PROXY_URL)
+            logger.info("Используется HTTP прокси.")
+
+    if REDIS_URL:
+        storage = RedisStorage.from_url(REDIS_URL)
+        log_redis_on_info()
+    else:
+        storage = MemoryStorage()
+        log_redis_off_info()
+
+    bot = Bot(token=BOT_TOKEN, session=session)
+    dp = Dispatcher(storage=storage)
 
     dp.include_router(messages.router)
     dp.include_router(containers.router)
